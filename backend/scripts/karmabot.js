5fdf28c9898c4e040b145fad340d57cd71b68003
Glitch (pixelbytedrive) <none>
2022-10-18 21:48:08 +0000

    🪟📄 Updated with Glitch

--- backend/scripts/karmabot.js
+++ backend/scripts/karmabot.js
@@ -6,8 +6,8 @@ app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
 });
 app.listen(6942);
-const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const { Discord, GatewayIntentBits, Client } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 client.on("ready", async () => {   
   client.user.setActivity("social credit system.", {
     type: "WATCHING",
@@ -17,9 +17,18 @@ client.on("ready", async () => {
 
 client.login(process.env.token3);
 client.on("messageCreate", async (message) => {
-   const upvote = client.emojis.cache.find(emoji => emoji.name === "upvote");
-   const downvote = client.emojis.cache.find(emoji => emoji.name === "downvote");
-   message.react(upvote);
-   message.react(downvote);
+   if (message.channel.id == 724045672839512139) {
+    if (message.attachments.size > 0) {
+      const upvote = client.emojis.cache.find(emoji => emoji.name === "upvote");
+      const downvote = client.emojis.cache.find(emoji => emoji.name === "downvote");
+      message.react(upvote);
+      message.react(downvote);
+    } else if (message.content.includes('https://')) {
+      const upvote = client.emojis.cache.find(emoji => emoji.name === "upvote");
+      const downvote = client.emojis.cache.find(emoji => emoji.name === "downvote");
+      message.react(upvote);
+      message.react(downvote);
+    } 
+   }
 });
 console.log("Active.");
