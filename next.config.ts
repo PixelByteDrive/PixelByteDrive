13a6474d850147906884e0cf7295d03d058603c7
Killian Dattilo <hello@pixelbytedrive.xyz>
2025-05-28 22:33:33 -0500

    Minor error correction

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/PixelByteDrive"
-  output: "export",
+  basePath: "/PixelByteDrive",
+  output: "export"
 };
 
 export default nextConfig;
